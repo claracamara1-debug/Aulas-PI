6096494d1c4165b4fdca705d0523646b93bfe43e
MARIA CLARA DA SILVA CÂMARA <20231204010066@ifrn.local>
2026-08-18 11:21:38 -0300

    colocando os arquivos iniciais de aplicação

---
+++ src/main/java/ifrn/pi/eventos/indexcontroller.java
@@ -0,0 +1,15 @@
+package ifrn.pi.eventos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexcontroller {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("chamou o método index");
+		return "Home";
+	}
+	
+}
