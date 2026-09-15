8335c41662733b2c072b385115620eb58f1372ff
MARIA CLARA DA SILVA CÂMARA <20231204010066@ifrn.local>
2026-09-15 15:30:03 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controlles/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controlles/IndexController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexcontroller {
+public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("chamou o método index");
-		return "Home";
+		System.out.println("Chamou o método index");
+		return "redirect:/eventos";
 	}
-	
+
 }
